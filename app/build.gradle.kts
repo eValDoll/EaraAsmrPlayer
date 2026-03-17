93c756c77eae38dbc27d35d766e0c9f7fc5eb88e
eValDoll <eValDoll@users.noreply.github.com>
2026-03-17 18:33:46 +0800

    chore: bump version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,8 +65,8 @@ android {
         applicationId = "com.asmr.player"
         minSdk = 24
         targetSdk = 34
-        versionCode = 10000
-        versionName = "1.0.0"
+        versionCode = 10001
+        versionName = "1.0.1"
         buildConfigField("String", "UPDATE_REPO_OWNER", "\"eValDoll\"")
         buildConfigField("String", "UPDATE_REPO_NAME", "\"EaraAsmrPlayer\"")
 
